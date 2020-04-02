75265292324e7edb3cadb244bfaf7e56f45d9152
TugcenurGunduz <tnurgunduz@hotmail.com>
2020-04-02 22:30:31 +0300

    12. video bitirildi. Ckeditor hakkımızda, referanslar ve iletişim kısmında kullanılıp anasayfaya adapte edilmiştir.

--- home/views.py
+++ home/views.py
@@ -7,5 +7,20 @@ from home.models import Setting
 
 def index(request):
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting}
+    context = {'setting': setting, 'page': 'home'}
     return render(request, 'index.html', context)
+
+def hakkimizda(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}   #{'setting': setting, 'page': 'hakkimizda'}
+    return render(request, 'hakkimizda.html', context)
+
+def referanslar(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}   #{'setting': setting, 'page': 'referanslar'}
+    return render(request, 'referanslarimiz.html', context)
+
+def iletisim(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}   #{'setting': setting, 'page': 'iletisim'}
+    return render(request, 'iletisim.html', context)
