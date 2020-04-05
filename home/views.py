a37f2c09806a72ad4429b3ef24e8c2b5fde30779
TugcenurGunduz <tnurgunduz@hotmail.com>
2020-04-05 17:03:15 +0300

    13. video bitirildi. Contact form tamamlandı. Bunun için bir model ve form oluşturduk, iletişim kısmında formu kaydetmek ve forma ulaşmak için fonksiyon yazıldı.

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
 
 
 def index(request):
@@ -21,6 +22,23 @@ def referanslar(request):
     return render(request, 'referanslarimiz.html', context)
 
 def iletisim(request):
+
+    if request.method == 'POST': #form post ediliyor
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage() #model ile bağlantı kur
+            data.name = form.cleaned_data['name'] #formdan bilgiyi al
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()#veritabanına kaydet
+            data.save() #veritabanına kaydet
+            messages.success(request, "Mesajınız başarı ile gönderilmiştir. Teşekkür ederiz")
+            return HttpResponseRedirect ('/iletisim')
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting}   #{'setting': setting, 'page': 'iletisim'}
+    form = ContactFormu()
+    context = {'setting': setting, 'form': form}   #{'setting': setting, 'page': 'iletisim'}
     return render(request, 'iletisim.html', context)
+
