01a72fef51f3fe58177158553de461279e19623e
tuğçe nur gündüz <tnurgunduz@hotmail.com>
2020-05-10 02:28:11 +0300

    23. video izlendi Kullanici Login ve Logout islemleri yapildi

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from unicodedata import category
 from django.contrib import messages
+from django.contrib.auth import logout, authenticate, login
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from home.models import Setting, ContactFormu, ContactFormMessage
@@ -95,3 +96,24 @@ def product_search(request):
             }
             return render(request, 'products_search.html', context)
     return HttpResponseRedirect('/')
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return HttpResponseRedirect('/')
+        else:
+            messages.warning(request, "Login Hatasi ! Kullanici Adi veya sifre yanlis")
+            return HttpResponseRedirect ('/login')
+
+    category = Category.objects.all()
+    context = { 'category': category,}
+    return render(request,'login.html',context)
--- project2/urls.py
+++ project2/urls.py
@@ -32,6 +32,8 @@ urlpatterns = [
     path('category/<int:id>/<slug:slug>/', views.category_products, name='category_products'),
     path('product/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
     path( 'search/', views.product_search, name='product_search' ),
+    path('logout/', views.logout_view, name='logout_view'),
+    path('login/', views.login_view, name='login_view'),
 ]
 if settings.DEBUG: # new
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
