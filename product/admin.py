dfb9e7398674a1453e5fe41719911d7949dcbee4
tuğçe nur gündüz <tnurgunduz@hotmail.com>
2020-05-09 18:30:17 +0300

    22. video izlendi Otel ve Kategori icin otomatik slug olusturulma yapildi

--- product/admin.py
+++ product/admin.py
@@ -20,6 +20,7 @@ class ProductAdmin(admin.ModelAdmin):
     readonly_fields = ('image_tag',)
     list_filter = ['status', 'category']
     inlines = [ProductImageInline]
+    prepopulated_fields = {'slug':('title',)}
 
 
 class ImagesAdmin(admin.ModelAdmin):
@@ -32,6 +33,7 @@ class CategoryAdmin2(DraggableMPTTAdmin):
     list_display = ('tree_actions', 'indented_title',
                     'related_products_count', 'related_products_cumulative_count')
     list_display_links = ('indented_title',)
+    prepopulated_fields = {'slug':('title',)}
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
--- product/models.py
+++ product/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ModelForm, TextInput, Textarea
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 from ckeditor_uploader.fields import RichTextUploadingField
 from mptt.fields import TreeForeignKey
@@ -16,7 +17,7 @@ class Category(MPTTModel):
     description = models.CharField(blank=True, max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
-    slug = models.SlugField()
+    slug = models.SlugField(null=False,unique=True)
     parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
@@ -36,8 +37,10 @@ class Category(MPTTModel):
 
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
-
     image_tag.short_description = 'Image'
+
+    def get_absolute_url(self):
+        return reverse('category_detail',kwargs={'slug': self.slug})
 
 
 class Product(models.Model):
@@ -53,7 +56,7 @@ class Product(models.Model):
     price = models.FloatField()
     amount = models.IntegerField()
     detail = RichTextUploadingField()
-    slug = models.SlugField(blank=True, max_length=150)
+    slug = models.SlugField(null=False,unique=True)
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
@@ -63,8 +66,10 @@ class Product(models.Model):
 
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
-
     image_tag.short_description = 'Image'
+
+    def get_absolute_url(self):
+        return reverse('product_detail',kwargs={'slug': self.slug})
 
 
 class Images(models.Model):
